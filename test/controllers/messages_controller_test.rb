1bf2451746bbfac64fd76ff52581f04d219889e5
JAHarris <JAHarris@localhost>
2016-07-14 18:50:22 -0400

    Added shirt images

---
+++ test/controllers/messages_controller_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class MessagesControllerTest < ActionController::TestCase
+	
+	test "succesful post" do
+  assert_difference 'ActionMailer::Base.deliveries.size', 1 do
+    post :create, message: {
+      name: 'cornholio',
+      email: 'cornholio@example.com',
+      subject: 'hi',
+      content: 'bai'
+    }
+  end
+
+  assert_redirected_to new_message_path
+  last_email = ActionMailer::Base.deliveries.last
+
+  assert_equal "hi", last_email.subject
+  assert_equal 'J2Jibbi@gmail.com', last_email.to[0]
+  assert_equal 'cornholio@example.com', last_email.from[0]
+  assert_match(/bai/, last_email.body.to_s)
+
+  ActionMailer::Base.deliveries.clear
+end
+end
